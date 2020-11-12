95fb40f4e8175fda5120ad969df5729f4bd93097
Gokul Alex <gokulsan@users.noreply.github.com>
2020-11-12 10:59:31 +0530

    Update network-counter.ts

--- Authenticator/test/network-counter.ts
+++ Authenticator/test/network-counter.ts
@@ -1,36 +1,36 @@
 import { Client, Provider, ProviderRegistry, Result } from "@blockstack/clarity";
 import { assert } from "chai";
 
-describe("hello world contract test suite", () => {
-  let helloWorldClient: Client;
+describe("network counter contract test suite", () => {
+  let networkCounterClient: Client;
   let provider: Provider;
 
   before(async () => {
     provider = await ProviderRegistry.createProvider();
-    helloWorldClient = new Client("SP3GWX3NE58KXHESRYE4DYQ1S31PQJTCRXB3PE9SB.hello-world", "hello-world", provider);
+    networkCounterClient = new Client("SP3GWX3NE58KXHESRYE4DYQ1S31PQJTCRXB3PE9SB.hello-world", "network-counter", provider);
   });
 
   it("should have a valid syntax", async () => {
-    await helloWorldClient.checkContract();
+    await networkCounterClient.checkContract();
   });
 
   describe("deploying an instance of the contract", () => {
     before(async () => {
-      await helloWorldClient.deployContract();
+      await networkCounterClient.deployContract();
     });
 
-    it("should return 'hello world'", async () => {
-      const query = helloWorldClient.createQuery({ method: { name: "say-hi", args: [] } });
-      const receipt = await helloWorldClient.submitQuery(query);
+    it("should return 'network counter'", async () => {
+      const query = networkCounterClient.createQuery({ method: { name: "say-hi", args: [] } });
+      const receipt = await networkCounterClient.submitQuery(query);
       const result = Result.unwrapString(receipt);
       assert.equal(result, "hello world");
     });
 
     it("should echo number", async () => {
-      const query = helloWorldClient.createQuery({
+      const query = networkCounterClient.createQuery({
         method: { name: "echo-number", args: ["123"] }
       });
-      const receipt = await helloWorldClient.submitQuery(query);
+      const receipt = await networkCounterClient.submitQuery(query);
       const result = Result.unwrapInt(receipt)
       assert.equal(result, 123);
     });
